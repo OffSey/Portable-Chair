4a83294b342e9e5482db59ca44716b9b0c522b87
OffSey <87211817+OffSey@users.noreply.github.com>
2026-03-17 20:55:50 +0100

    Add fxmanifest for Portable Chair resource

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'OffSey'
+description 'Portable Chair'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
